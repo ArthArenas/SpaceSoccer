1cad4db7d70b07d8495761b940e9ac110775c823
SergioSanchez <sergiohsanchezmtz@gmail.com>
2018-03-06 18:30:26 -0600

    Adding sprites and the enemy class

--- SpaceSoccer/src/videogame/Enemy.java
+++ SpaceSoccer/src/videogame/Enemy.java
@@ -5,10 +5,64 @@
  */
 package videogame;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  *
- * @author arena
+ * @author Arturo Arenas Esparza (A00820982)
+ * @author Sergio Sanchez Martinez (A00809693)
  */
-public class Enemy {
+public class Enemy extends Item{
+
+    private Game game;  // Reference to the game
+    private boolean front; // Determinate if the enemy can shot
+    private int type; // Represent the shape of the enemy
     
+    /**
+     * Constructor of the bar
+     * @param x the <b>x</b> position of the player
+     * @param y the <b>y</b> position of the player
+     * @param width the width of the player
+     * @param height the height of the player
+     * @param game the copy of the game
+     */
+    public Enemy(int x, int y, int width, int height, Game game) {
+        super(x, y, width, height);
+        this.game = game;
+        front = false;
+    }
+
+     /**
+     * Getter for the front
+     * @return the status of an enemy to
+     */    
+    public boolean isFront() {
+        return front;
+    }
+
+    
+    /**
+     * Setter for the front
+     * @param front the status of the enemy to shot
+     */
+    public void setFront(boolean front) {
+        this.front = front;
+    }
+    
+    /**
+     * Update the sttributes of the bar
+     */
+    @Override
+    public void tick() {
+    }
+
+    /**
+     * Paints the bar
+     * @param g the grphics to paint the bar
+     */
+    @Override
+    public void render(Graphics g) {
+        // g.drawImage(Assets.player, getX(), getY(), getWidth(), getHeight(), null);
+    }
 }
